663b270d201d84696c718c95846a9fc74445bc40
José Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2011-12-20 10:06:02 -0300

    Added licensing headers to source files.

--- autoload.php
+++ autoload.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Clinner library.
+ *
+ * (c) José Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (false === class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', false)) {
     require_once __DIR__ . '/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 }
--- src/Clinner/Command/Command.php
+++ src/Clinner/Command/Command.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Clinner library.
+ *
+ * (c) José Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Clinner\Command;
 
 use Clinner\Command\CommandInterface;
@@ -270,6 +279,11 @@ class Command implements CommandInterface
      */
     public function pipe($anotherCommand)
     {
+        if ($this === $anotherCommand) {
+            // Cannot pipe a command to itself, need to clone it
+            $anotherCommand = clone $anotherCommand;
+        }
+
         $this->_next = $anotherCommand;
 
         return $this;
