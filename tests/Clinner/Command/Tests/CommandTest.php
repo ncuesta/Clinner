71f8f6f091d36bc35bedbbdd9cbe74d33bf7ee36
José Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2011-12-31 20:02:50 -0300

    Updated Command class tests.

--- tests/Clinner/Command/Tests/CommandTest.php
+++ tests/Clinner/Command/Tests/CommandTest.php
@@ -21,7 +21,35 @@ use Clinner\Command\Command;
  */
 class CommandTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFake()
+    public function testStaticCreateNoArgsNoOpts()
     {
+        $name = 'ls';
+
+        $command = Command::create($name);
+
+        $this->assertInstanceOf('\\Clinner\\Command\\Command', $command);
+        $this->assertAttributeEquals($name, '_name', $command);
+        $this->assertAttributeInstanceOf('\\Clinner\\ValueHolder', '_arguments', $command);
+        $this->assertAttributeInstanceOf('\\Clinner\\ValueHolder', '_options', $command);
+        $this->assertAttributeEmpty('_next', $command);
+        $this->assertAttributeEmpty('_exitCode', $command);
+        $this->assertAttributeEmpty('_output', $command);
+    }
+
+    public function testStaticCreateWithArgsAndOpts()
+    {
+        $name = 'ls';
+        $args = array('first' => 'value', 'second' => 'second value');
+        $opts = array('some_option' => 'some value');
+
+        $command = Command::create($name, $args, $opts);
+
+        $this->assertInstanceOf('\\Clinner\\Command\\Command', $command);
+        $this->assertAttributeEquals($name, '_name', $command);
+        $this->assertAttributeInstanceOf('\\Clinner\\ValueHolder', '_arguments', $command);
+        $this->assertAttributeInstanceOf('\\Clinner\\ValueHolder', '_options', $command);
+        $this->assertAttributeEmpty('_next', $command);
+        $this->assertAttributeEmpty('_exitCode', $command);
+        $this->assertAttributeEmpty('_output', $command);
     }
 }
